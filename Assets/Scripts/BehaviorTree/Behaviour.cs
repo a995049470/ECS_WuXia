014f5678c10f10851a0ca44347f812bf36f2b234
a995049470 <17855834269@163.com>
2020-07-03 00:17:12 +0800

    行为树

---
+++ Assets/Scripts/BehaviorTree/Behaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BT
+{
+    public enum BTStatus
+    {
+        Invalid,   //初始状态
+        Success,   //成功
+        Failure,   //失败
+        Running,   //运行
+        //Aborted,   //终止
+    }
+
+    public class BehaviorTree
+    {
+        private Behaviour m_root;
+        
+    }
+
+    //所有行为的父节点
+    public abstract class Behavior
+    {
+        protected abstract BTStatus Tick(Agent agent);
+        protected abstract void OnInitialize();
+        protected abstract BTStatus Update();
+        protected abstract void OnOnTerminate(BTStatus status);
+
+        public BTStatus GetBTStatus() { return m_btStatus; }
+        private BTStatus m_btStatus;
+    }
+
+
+
+}
+
